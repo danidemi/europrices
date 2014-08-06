a4ab7ade9101408c9552aa9925e29d1186c6e9f1
danidemi <demichelis@danidemi.com>
2014-08-06 17:40:51 +0200

    Sample of basic controller

---
+++ src/main/java/com/danidemi/europrice/web/controller/api/PocApi.java
@@ -0,0 +1,18 @@
+package com.danidemi.europrice.web.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/pocapi")
+public class PocApi {
+
+	@RequestMapping(value="/version", method=RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public String version() {
+		return "v0.0.001";
+	}
+	
+}
