a5bfb5f76508a59a67255e787fd5392f20ece686
danidemi <demichelis@danidemi.com>
2014-08-06 17:46:34 +0200

    Example of api returning shops

--- src/main/java/com/danidemi/europrice/web/controller/api/PocApi.java
+++ src/main/java/com/danidemi/europrice/web/controller/api/PocApi.java
@@ -1,9 +1,14 @@
 package com.danidemi.europrice.web.controller.api;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.danidemi.europrice.db.Shop;
 
 @Controller
 @RequestMapping("/pocapi")
@@ -14,5 +19,22 @@ public class PocApi {
 	public String version() {
 		return "v0.0.001";
 	}
+	
+	@RequestMapping(value="/shops", method=RequestMethod.GET, produces="application/json")
+	@ResponseBody
+	public List<Shop> shops() {
+		
+		Shop shop1 = new Shop();
+		shop1.setId(10L); shop1.setName("the shop");
+		
+		Shop shop2 = new Shop();
+		shop2.setId(20L); shop2.setName("de zop");
+		
+		Shop shop3 = new Shop();
+		shop3.setId(30L); shop3.setName("und zop");
+		
+		return Arrays.asList( shop1, shop2, shop3 );
+		
+	}	
 	
 }
