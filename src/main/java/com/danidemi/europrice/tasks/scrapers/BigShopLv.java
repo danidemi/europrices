2de49cfc5861707088989675638a55aa67f3f418
danidemi <demichelis@danidemi.com>
2014-08-17 16:53:30 +0200

    New shop added

---
+++ src/main/java/com/danidemi/europrice/tasks/scrapers/BigShopLv.java
@@ -0,0 +1,52 @@
+package com.danidemi.europrice.tasks.scrapers;
+
+import static com.danidemi.europrice.screenscraping.Scrape.forEachItem;
+import static com.danidemi.europrice.screenscraping.Scrape.forEachPageWithNextLinkDo;
+import static com.danidemi.europrice.screenscraping.Scrape.inSequence;
+import static com.danidemi.europrice.screenscraping.Scrape.search;
+
+import org.openqa.selenium.By;
+
+import com.danidemi.europrice.poc.pricegrabber.Callback;
+import com.danidemi.europrice.poc.pricegrabber.Request;
+import com.danidemi.europrice.screenscraping.GoToUrl;
+import com.danidemi.europrice.screenscraping.ScrapeAction;
+import com.danidemi.europrice.screenscraping.ScrapeContext;
+import com.danidemi.europrice.screenscraping.Scrapers;
+
+public class BigShopLv extends AbstractShopScraper {
+
+	public BigShopLv() {
+		super("BIGSHOP-LV");
+	}
+
+	@Override
+	protected ScrapeAction buildScrapeAction(ScrapeContext ctx,Request request, Callback callback) throws Exception {
+
+		action.setDescriptionSelector(By.cssSelector("H3"));
+		action.setDescriptionScraper(Scrapers.text());
+		
+		action.setDetailUrlSelector(By.cssSelector("A"));
+		action.setDetailUrlScraper(Scrapers.attribute("href"));
+		
+		action.setPriceSelector(By.cssSelector(".price"));
+		action.setPriceScraper(Scrapers.text());
+		
+		action.setShopName(getShopName());
+		
+		return
+				inSequence( new GoToUrl("http://www.bigshop.lv/") )
+				.then( 
+						search( request.getSearchTerm() , 
+						By.cssSelector("#search > input"), 
+						By.cssSelector("#search > a")))
+				.then( forEachPageWithNextLinkDo(
+						By.cssSelector(".next"), 
+						forEachItem(
+								By.cssSelector("div.item"), action))
+						)
+								;
+		
+	}
+
+}
