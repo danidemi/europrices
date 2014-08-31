92a39db15592f4969cdd76b27fdba7b65133717e
danidemi <demichelis@danidemi.com>
2014-08-31 23:44:25 +0200

    changing port of phantomjs

--- src/main/java/com/danidemi/europrice/screenscraping/PhantomjsFactory.java
+++ src/main/java/com/danidemi/europrice/screenscraping/PhantomjsFactory.java
@@ -1,14 +1,19 @@
 package com.danidemi.europrice.screenscraping;
 
+import java.io.File;
+
 import org.openqa.selenium.Proxy;
+import org.openqa.selenium.browserlaunchers.Proxies;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PhantomjsFactory implements ScrapeContextFactory {
-	
+		
 	final Logger log = LoggerFactory.getLogger(PhantomjsFactory.class);	
+	
 	
 	private String proxy;
 	private int port;
@@ -53,8 +58,47 @@ public class PhantomjsFactory implements ScrapeContextFactory {
         	capabilities.setCapability("proxy", proxy);
         }
         
+        capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] { "localhost", "port=8080" });
         
-        final PhantomJSDriver driver = new PhantomJSDriver(capabilities);
+        
+        //final PhantomJSDriver driver = new PhantomJSDriver(capabilities);
+        
+
+        PhantomJSDriverService.Builder b = new PhantomJSDriverService.Builder();
+        b.usingGhostDriver( new File(pathToPhantomJsExecutable) );
+        b.usingCommandLineArguments(new String[]{"--ignore-ssl-errors=yes"});
+        b.usingPhantomJSExecutable( new File(pathToPhantomJsExecutable) );
+        b.usingPort(8080);
+        PhantomJSDriverService service = b.build();
+        
+        final PhantomJSDriver driver = new PhantomJSDriver(service, capabilities);
+        
+//        // Look for Proxy configuration within the Capabilities
+//        Proxy proxy = null;
+//        if (capabilities != null) {
+//        	proxy = Proxies.extractProxy(capabilities);
+//        }
+//        // Find PhantomJS executable
+//        File phantomjsfile = findPhantomJS(
+//        		capabilities, 
+//        		PhantomJSDriverService.PHANTOMJS_DOC_LINK, 
+//        		PhantomJSDriverService.PHANTOMJS_DOWNLOAD_LINK);
+//
+//        // Find GhostDriver main JavaScript file
+//        File ghostDriverfile = findGhostDriver(capabilities, PhantomJSDriverService.GHOSTDRIVER_DOC_LINK, PhantomJSDriverService.GHOSTDRIVER_DOWNLOAD_LINK);
+//
+//        // Build & return service
+//        return new Builder().usingPhantomJSExecutable(phantomjsfile)
+//                .usingGhostDriver(ghostDriverfile)
+//                .usingAnyFreePort()
+//                .withProxy(proxy)
+//                .withLogFile(new File(PhantomJSDriverService.PHANTOMJS_DEFAULT_LOGFILE))
+//                .usingCommandLineArguments(
+//                        findCLIArgumentsFromCaps(capabilities, PhantomJSDriverService.PHANTOMJS_CLI_ARGS))
+//                .usingGhostDriverCommandLineArguments(
+//                        findCLIArgumentsFromCaps(capabilities, PhantomJSDriverService.PHANTOMJS_GHOSTDRIVER_CLI_ARGS))
+//                .build();
+//        final PhantomJSDriver driver = new PhantomJSDriver(service, capabilities);
         
         
         Runtime.getRuntime().addShutdownHook(new Thread(){
