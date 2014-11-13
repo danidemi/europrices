654b18284f90aea9619f6bb3bf32e12ed813b921
danidemi <demichelis@danidemi.com>
2014-11-13 16:56:22 +0100

    Removed unused java 8 imports

--- europrices-webapp/src/main/java/com/danidemi/europrice/web/controller/api/SearchTermSplitter.java
+++ europrices-webapp/src/main/java/com/danidemi/europrice/web/controller/api/SearchTermSplitter.java
@@ -1,11 +1,7 @@
 package com.danidemi.europrice.web.controller.api;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.cglib.core.CollectionUtils;
 
 public class SearchTermSplitter {
 
