9218abe6da56345d86c9d0361b95b80f982bdbab
danidemi <demichelis@danidemi.com>
2014-11-18 19:50:12 +0100

    Jasmine added

---
+++ europrices-webapp/src/main/resources/webapp/js/spec/europrices.js
@@ -0,0 +1,8 @@
+describe("Europrices", function() {
+	it("contains spec with an expectation", function() {
+    		expect(true).toBe(true);
+  	});
+  	it("and the opposite", function() {
+    		expect(true).not.toBe(false);
+  	});
+});
